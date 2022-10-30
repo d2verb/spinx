309d1e6817d7539bb45047362651444a4a5a7e1e
d2verb <licprog73@gmail.com>
2022-10-31 01:48:47 +0900

    Update examples/manual.ts to show we can call start() multiple times.

--- examples/manual.ts
+++ examples/manual.ts
@@ -2,12 +2,20 @@ import { Spinner } from "../mod.ts";
 
 async function run(spinner: Spinner) {
   spinner.start();
+
+  // We can invoke start() multiple times but the only first one is effective.
+  spinner.start();
+  spinner.start();
+  spinner.start();
+
   const p = Deno.run({
     cmd: ["sleep", "2"],
     stdout: "piped",
   });
   await p.status();
   await p.close();
+
+  // stop() clears internal state of the spinner and makes start() effective again.
   spinner.stop();
 }
 
